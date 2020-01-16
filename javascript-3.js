15c194f5536e687cf779e119dddc7e7cff95e380
MalkovVladimir <kristinazubova31@gmail.com>
2020-01-16 09:48:22 +0300

    getPath

---
+++ javascript-3.js
@@ -0,0 +1,33 @@
+// getPath - поиск уникального селектора
+// Написать алгоритм и функцию `getPath()`, находяющую уникальный css-селектор для элемента в документе.
+// Уникальный селектор может быть использован `document.querySelector()` и возвращать исходный элемент.
+// `document.querySelectorAll()`, вызванный с этим селектором, не должен находить никаких элементов, кроме исходного.
+
+function getPath (elem) {
+
+  let path;
+
+  if (elem.id) {
+    return '#' + elem.id;
+  }
+  
+  if (elem.classList.length) {
+
+    for (let i = 0; i < elem.classList.length; i++ ) {
+      let cssClass = elem.classList[i];
+
+      if (document.querySelectorAll('.' + cssClass).length === 1) {
+        return '.' + cssClass;
+      }
+    }
+  }
+  
+  if (elem.tagName === 'BODY') {
+    return 'body'
+  }
+  else {
+    let childIndex = Array.prototype.indexOf.call(elem.parentElement.children, elem) + 1;
+    path = getPath(elem.parentElement) + ' > ' + elem.tagName.toLowerCase() + ':nth-child(' + childIndex + ')';
+  }
+  return path;
+}
