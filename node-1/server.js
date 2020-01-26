9b2e653a3d401a8c4c66b246e5bd992a9a36bfd1
MalkovVladimir <kristinazubova31@gmail.com>
2020-01-26 13:47:29 +0300

    request and server are created

---
+++ node-1/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+  await new Promise (resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.end('{"test": 1}');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
