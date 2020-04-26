3615363375b6e08f14bd901d41322912d0168228
Kristina Mal'kova <kristinazubova31@gmail.com>
2020-04-26 20:38:26 +0300

    words-app created

---
+++ angular/words-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecentlyAddedComponent } from './recently-added/recently-added.component';
+import { ExercisesComponent } from './exercises/exercises.component';
+import { SettingsComponent } from './settings/settings.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecentlyAddedComponent,
+    ExercisesComponent,
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
